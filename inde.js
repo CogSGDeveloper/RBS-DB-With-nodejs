c5b0d77cb443f7a9795f5fdf652c66ddf625a90e
Sudhir Govekar <sudhir.x.govekar@gsk.com>
2022-07-10 07:14:39 +0530

    added try catch block

--- inde.js
+++ inde.js
@@ -1,19 +1,22 @@
 var mysql = require('mysql');
-
-var connection = mysql.createConnection({
-  host     : 'database-1.cgnz8tkdfkk9.us-west-2.rds.amazonaws.com',
-  user     : 'admin',
-  password : 'admin',
-  port     : 3306
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error('Database connection failed: ' + err.stack);
-    return;
-  }
-
-  console.log('Connected to database.');
-});
-
-connection.end();
+try {
+  var connection = mysql.createConnection({
+    host     : 'database-1.cgnz8tkdfkk9.us-west-2.rds.amazonaws.com',
+    user     : 'admin',
+    password : 'admin',
+    port     : 3306
+  });
+  
+  connection.connect(function(err) {
+    if (err) {
+      console.error('Database connection failed: ' + err.stack);
+      return;
+    }
+  
+    console.log('Connected to database.');
+  });
+  
+  connection.end();
+} catch (error) {
+  console.log(error);
+}
